85fcd99eb4d6553a1d69a66b0bdf0a1549378f03
thrilokprakashh <thrilokprakashan@gmail.com>
2024-09-05 02:51:10 +0530

    list added

--- lib/view/homescreen/home_screen.dart
+++ lib/view/homescreen/home_screen.dart
@@ -10,6 +10,119 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List videoCardData = [
+    {
+      "rating": "4.5",
+      "image":
+          "https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&w=1200",
+      "time": "15:10",
+      "title": "How to make sushi at home",
+      "description": "By Niki Samantha",
+      "dp":
+          "https://images.pexels.com/photos/13733587/pexels-photo-13733587.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load",
+    },
+    {
+      "rating": "4.8",
+      "image":
+          "https://images.pexels.com/photos/1600727/pexels-photo-1600727.jpeg?auto=compress&cs=tinysrgb&w=1200",
+      "time": "10:45",
+      "title": "The Ultimate Vegan Burger",
+      "description": "By Chef Leo",
+      "dp":
+          "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load",
+    },
+    {
+      "rating": "4.6",
+      "image":
+          "https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?auto=compress&cs=tinysrgb&w=1200",
+      "time": "12:22",
+      "title": "Easy Pasta Recipes",
+      "description": "By Maria Johnson",
+      "dp":
+          "https://images.pexels.com/photos/1342460/pexels-photo-1342460.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load",
+    },
+    {
+      "rating": "4.7",
+      "image":
+          "https://images.pexels.com/photos/46239/salmon-dish-food-meal-46239.jpeg?auto=compress&cs=tinysrgb&w=1200",
+      "time": "18:00",
+      "title": "Perfect Grilled Salmon",
+      "description": "By Alex Browne",
+      "dp":
+          "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load",
+    },
+    {
+      "rating": "4.9",
+      "image":
+          "https://images.pexels.com/photos/825661/pexels-photo-825661.jpeg?auto=compress&cs=tinysrgb&w=1200",
+      "time": "14:50",
+      "title": "Authentic Italian Pizza",
+      "description": "By Luca Santoro",
+      "dp":
+          "https://images.pexels.com/photos/412840/pexels-photo-412840.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load",
+    },
+    {
+      "rating": "4.3",
+      "image":
+          "https://images.pexels.com/photos/103124/pexels-photo-103124.jpeg?auto=compress&cs=tinysrgb&w=1200",
+      "time": "9:15",
+      "title": "Quick Breakfast Ideas",
+      "description": "By Emily Grace",
+      "dp":
+          "https://images.pexels.com/photos/774558/pexels-photo-774558.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load",
+    },
+    {
+      "rating": "4.4",
+      "image":
+          "https://images.pexels.com/photos/1624487/pexels-photo-1624487.jpeg?auto=compress&cs=tinysrgb&w=1200",
+      "time": "17:30",
+      "title": "Mastering the Art of Tacos",
+      "description": "By Diego Ramirez",
+      "dp":
+          "https://images.pexels.com/photos/1367192/pexels-photo-1367192.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load",
+    },
+    {
+      "rating": "4.2",
+      "image":
+          "https://images.pexels.com/photos/267308/pexels-photo-267308.jpeg?auto=compress&cs=tinysrgb&w=1200",
+      "time": "11:40",
+      "title": "Making French Pastries",
+      "description": "By Claire Dupont",
+      "dp":
+          "https://images.pexels.com/photos/17613679/pexels-photo-17613679/free-photo-of-a-woman-sitting-on-a-blanket-in-a-field.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load",
+    },
+    {
+      "rating": "4.7",
+      "image":
+          "https://images.pexels.com/photos/1059905/pexels-photo-1059905.jpeg?auto=compress&cs=tinysrgb&w=1200",
+      "time": "16:50",
+      "title": "Mediterranean Salad Bowls",
+      "description": "By Sophia Lee",
+      "dp":
+          "https://images.pexels.com/photos/769289/pexels-photo-769289.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load",
+    },
+    {
+      "rating": "4.5",
+      "image":
+          "https://images.pexels.com/photos/406152/pexels-photo-406152.jpeg?auto=compress&cs=tinysrgb&w=1200",
+      "time": "22:10",
+      "title": "Homemade Ramen Noodles",
+      "description": "By Kenji Tanaka",
+      "dp":
+          "https://images.pexels.com/photos/13733587/pexels-photo-13733587.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load",
+    },
+    {
+      "rating": "4.9",
+      "image":
+          "https://images.pexels.com/photos/887853/pexels-photo-887853.jpeg?auto=compress&cs=tinysrgb&w=1200",
+      "time": "13:30",
+      "title": "Delicious Chocolate Desserts",
+      "description": "By Bella Noir",
+      "dp":
+          "https://images.pexels.com/photos/3720429/pexels-photo-3720429.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load",
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -66,18 +179,17 @@ class _HomeScreenState extends State<HomeScreen> {
               itemBuilder: (context, index) => Column(
                     children: [
                       customVideoCard(
-                        rating: "4,5",
-                        image:
-                            "https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&w=1200",
-                        time: "15:10",
-                        title: "How to make sushi at home",
-                        description: "By Niki Samantha ",
-                        dp: "https://images.pexels.com/photos/13733587/pexels-photo-13733587.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load",
+                        rating: videoCardData[index]["rating"],
+                        image: videoCardData[index]["image"],
+                        time: videoCardData[index]["time"],
+                        title: videoCardData[index]["title"],
+                        description: videoCardData[index]["description"],
+                        dp: videoCardData[index]["dp"],
                       ),
                     ],
                   ),
               separatorBuilder: (context, index) => SizedBox(width: 16),
-              itemCount: 10),
+              itemCount: 11),
         ),
       ],
     );
